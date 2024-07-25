f7f814e50f484b4caf53582c9a5907827338fa2f
bc1001 <67504941+bc1001@users.noreply.github.com>
2024-07-25 17:31:16 +0800

    Update Tweak.h

--- Sources/CardculatorC/include/Tweak.h
+++ Sources/CardculatorC/include/Tweak.h
@@ -1,7 +1,6 @@
 #import <UIKit/UIKit.h>
 #import <SpringBoard/SpringBoard.h>
 #import <libactivator/libactivator.h>
-#include "RemoteLog.h"
 
 @interface CCUIModularControlCenterOverlayViewController: UIViewController
 -(void)dismissAnimated:(BOOL)arg1 withCompletionHandler:(/*^block*/id)arg2 ;
